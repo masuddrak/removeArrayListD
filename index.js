530fb33fa6fc8fbfcdc380eb7259a406beb38b36
masudrana <masudranaacb@gmail.com>
2022-02-02 23:20:38 +0600

    total miles of animals

--- index.js
+++ index.js
@@ -1,11 +1,25 @@
-const products=[
-    {nsme:'phone', price:10, quantity:2},
-    {nsme:'shirt', price:5, quantity:3},
-    {nsme:'laptop', price:2, quantity:1},
-]
-let totalPrice=0;
-for(const product of products){
-    let totalProductPrice=product.price*product.quantity;
-    totalPrice=totalPrice+totalProductPrice;
+function animals(mils){
+    const perMils5=5;
+    const perMils10=10;
+    const perMils10Plus=15;
+    if(mils<=5){
+        const total5Mils=mils*perMils5;
+        return total5Mils;
+    }
+    else if(mils<=10){
+        const frist5Mils=5*perMils5;
+        const restMils=mils-5;
+        const total10mils=restMils*perMils10;
+        const total10milsAnimals=total10mils+frist5Mils;
+        return total10milsAnimals;
+    }
+    else{
+        const second10Mils=10*perMils10;
+        const restMils=mils-10;
+        const total10Plus=restMils*perMils10Plus;
+        const total10MilsPlus=total10Plus+second10Mils;
+        return total10MilsPlus;
+    }
 }
-console.log('your total shope price =',totalPrice);
+const totals=animals(12);
+console.log(totals);
