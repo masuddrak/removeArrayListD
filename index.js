1b95ffd36fa0aa61b54c03735b0bf037c0ef59f4
masudrana <masudranaacb@gmail.com>
2022-02-02 21:14:05 +0600

    2elemt object addition

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
 const products=[
-    {nsme:'phone', price:1000,},
-    {nsme:'shirt', price:500,},
-    {nsme:'laptop', price:20000,}
+    {nsme:'phone', price:10, quantity:2},
+    {nsme:'shirt', price:5, quantity:3},
+    {nsme:'laptop', price:2, quantity:1},
 ]
 let totalPrice=0;
 for(const product of products){
-    totalPrice=totalPrice+product.price;
+    let totalProductPrice=product.price*product.quantity;
+    totalPrice=totalPrice+totalProductPrice;
 }
 console.log('your total shope price =',totalPrice);
