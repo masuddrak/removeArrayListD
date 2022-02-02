596d1204b2a945b24dc5ccb711bea650c0d5085b
masudrana <masudranaacb@gmail.com>
2022-02-02 14:39:16 +0600

    just a fun game

--- index.js
+++ index.js
@@ -1,14 +1,12 @@
-function woodCalculator(chair,table,bed){
-    const perChairWood=3;
-    const perTableWood=10;
-    const perBedWood=50;
 
-    const chairQuantityWood=chair*perChairWood;
-    const tableQuantityWood=table*perTableWood;
-    const bedQuantityWood=bed*perBedWood;
-
-    const totalWood=chairQuantityWood+tableQuantityWood+bedQuantityWood;
-    return totalWood;
+for(let i=1; i<=30; i++){
+    if(i%3==0 && i%5==0){
+        console.log("its best",i);
+    }
+    else if(i%3==0){
+        console.log('good',i)
+    }
+    else{
+        console.log(i);
+    }
 }
-const result=woodCalculator(0,0,2);
-console.log(result);
